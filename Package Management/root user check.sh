3c7d134e54cb5742b56616fe9c0a39c98e2939ef
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-11-30 23:57:30 +0330

    Update root user check.sh

--- Package Management/root user check.sh
+++ Package Management/root user check.sh
@@ -4,6 +4,10 @@ if test "`id -u`" -ne 0
 	echo "You need to run this script as root!" 
 	exit 
 fi
-
+---------------
+if [ "`id -u`" != "0" ];then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
 
