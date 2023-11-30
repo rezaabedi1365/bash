40101ba7d61c48dc3a628e55c98ca652ec0ae7ff
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-01 00:01:13 +0330

    Update and rename root user check.sh to function_root user check .sh

--- Package Management/function_root user check .sh
+++ Package Management/function_root user check .sh
@@ -1,13 +1,16 @@
 #Checks if you are root or not 
+user_check () {
 if test "`id -u`" -ne 0
 	then 
 	echo "You need to run this script as root!" 
 	exit 
 fi
+}
 ---------------
+user_check () {
 if [ "`id -u`" != "0" ];then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
-
+}
 
