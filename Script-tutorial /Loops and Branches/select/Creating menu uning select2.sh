6bac272b9f813fa3355bf969089ef43d9a5d9787
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-09 10:26:55 +0330

    Update Creating menu uning select2.sh

--- Script-tutorial /Loops and Branches/select/Creating menu uning select2.sh
+++ Script-tutorial /Loops and Branches/select/Creating menu uning select2.sh
@@ -1,14 +1,21 @@
 #Reza
 
 #!/bin/bash
-
-PS3='Choose your web server: ' # Sets the prompt string.                                
-
-echo
-select webserver in "nginx" "apache2" 
-do
-  echo 
-  apt install $vegetable
-  break  # What happens if there is no 'break' here?
+PS3='Choose your OS "  # Sets the prompt string.'                                
+select OS in "Ubuntu" "Centos" "Alpine" "Debian"
+do 
+case $OS in 
+    "Ubuntu|Debian"
+     apt install nginx
+    ;;
+    "Centos"
+      yum install nginx
+    ;;
+    "Alpine"
+      apk add nginx
+    ;;
+  *)
+    break;
+  esac
 done
-exit
+
