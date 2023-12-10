b9d2cfbf1059940cedc698acf77e1ba323c7d8ea
Reza Abedi <117336743+rezaabedi1365@users.noreply.github.com>
2023-12-10 15:58:40 +0330

    Backup_simple.sh

--- File and Folder Management/Backup/Backup_simple.sh
+++ File and Folder Management/Backup/Backup_simple.sh
@@ -1,12 +1,16 @@
 # https://mac-blog.org.ua/bash-backup-restore-script/
 
+
 #!/bin/sh
 
-# Backup  directory /root/script/  to file backup-date to /root
-/bin/tar -cpzf /root/backup-`date +%Y-%m-%d`.tar.gz /root/scripts/
+# Backup www directory to file backup-date  in /home 
+/bin/tar -cpzf /home/backups/www-`date +%Y-%m-%d`.tar.gz /home/example/www/
+
+
 
 # Backup database
 /usr/bin/mysqldump --databases -u example --password=example example > /home/example/backups/db-`date +%Y-%m-%d`.sql
 
+
 # Delete old backups
-/usr/bin/find /root/ -mtime +30 -delete
+/usr/bin/find /home/example/backups/ -mtime +30 -delete
